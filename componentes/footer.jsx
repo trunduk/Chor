450b6610b1865faeb5834c72d731ebb43d1d389c
trunduk <116894599+trunduk@users.noreply.github.com>
2023-03-03 18:09:48 +0300

    Add files via upload

---
+++ componentes/footer.jsx
@@ -0,0 +1,8 @@
+ export const Footer = () => {
+   return(
+   <footer>
+    <div>Все права защищены &copy;</div>
+    </footer>
+
+   )
+ }
---
+++ componentes/header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return(
+        <header>
+            <div>
+              <span class="logo">Eclipse Shope</span> 
+              <div className="presentetion"></div> 
+            </div>
+
+        </header>
+    )
+}
