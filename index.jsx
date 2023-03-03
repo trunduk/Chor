284d1e7418748e8002f053755d14b91996b05785
trunduk <116894599+trunduk@users.noreply.github.com>
2023-03-03 18:08:51 +0300

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,13 @@
+import { Header } from "@/componentes/header";
+import { Footer } from "@/componentes/footer";
+
+const Home = () => {
+    return(
+        <div className="wrapper">
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
